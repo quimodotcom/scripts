00a29e76adfe75605830f04a74dc89927c57aedd
quimodotcom <evandonald01@gmail.com>
2022-01-12 16:53:05 +0000

    Update client-install-raspberry-pi.sh

--- client-install-raspberry-pi.sh
+++ client-install-raspberry-pi.sh
@@ -16,7 +16,7 @@
 # Installer Script for Raspberry Pi 
 
 VERSION=2.11
-DOWNLOAD_LINK=https://github.com/stream-pi/client/releases/download/1.0.0-EA%2B4-SNAPSHOT/stream-pi-client-linux-arm32-1.0.0-EA+4-SNAPSHOT-executable.zip
+DOWNLOAD_LINK= https://github.com/stream-pi/client/releases/download/2.0.0-SNAPSHOT/stream-pi-client-linux-arm32-2.0.0-SNAPSHOT-executable.zip
 CONFIG=/boot/config.txt
 NINE_NINE_RULES_FILE=/etc/udev/rules.d/99-com.rules
 BACKLIGHT_PERMISSIONS_RULES_FILE=/etc/udev/rules.d/backlight-permissions.rules
